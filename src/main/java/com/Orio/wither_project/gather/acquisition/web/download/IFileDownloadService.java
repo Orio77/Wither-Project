e04c2b78ad504338993b2664c0ad5e89ccc1c0b9
Orio <maciejll@wp.pl>
2025-03-13 18:41:29 +0100

    Remove unused service interfaces and implementations for query, file download, scraping, and data processing

--- src/main/java/com/Orio/wither_project/gather/acquisition/web/download/IFileDownloadService.java
+++
@@ -1,8 +0,0 @@
-package com.Orio.wither_project.gather.acquisition.web.download;
-
-import java.util.List;
-
-public interface IFileDownloadService {
-
-    void download(List<String> links, String query);
-}
